0d9be62a183435b2d59ac332720aed23befa4c58
Ezuunuu <68217692+Ezuunuu@users.noreply.github.com>
2025-02-15 01:33:52 +0900

    first create

---
+++ app/src/main/java/com/ezuunuu/ble_scanner_connector/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.ezuunuu.ble_scanner_connector
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ezuunuu.ble_scanner_connector.ui.theme.Ble_scanner_connectorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Ble_scanner_connectorTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Ble_scanner_connectorTheme {
+        Greeting("Android")
+    }
+}
